26c5d984afd988555f0a64a45a476a2255752a4f
Nikita U <nikitajc@mail.ru>
2023-01-23 16:25:46 +0300

    Fixed

--- src/test/java/CardPlanningTest.java
+++ src/test/java/CardPlanningTest.java
@@ -1,21 +1,17 @@
 import com.codeborne.selenide.Condition;
-import com.github.javafaker.Faker;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
+
 import java.time.Duration;
+
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CardPlanningTest {
 
-    Faker faker;
-    DateClass date = new DateClass();
     String deleteString = Keys.chord(Keys.CONTROL, "a") + Keys.DELETE;
-    String generatedDate = DataGenerator.registration.generateInfo("ru").getMeetingDate();
-    String generatedName = DataGenerator.registration.generateInfo("ru").getName();
-    String generatedPhone = DataGenerator.registration.generateInfo("ru").getPhone();
+    DataGenerator.UserInfo validUser = DataGenerator.registration.generateInfo("ru");
 
     @BeforeEach
     void openChrome() {
@@ -23,22 +19,11 @@ public class CardPlanningTest {
     }
 
     @Test
-    void validValueTestWithFaker() {
+    @Order(1)
+    void test_ToReplanNotificationTrigger() {
         $("[data-test-id=city] input").setValue("Краснодар");
         $("[data-test-id=date] input").setValue(deleteString);
-        $("[data-test-id=date] input").setValue(generatedDate);
-        $("[data-test-id=name] input").setValue(generatedName);
-        $("[data-test-id=phone] input").setValue(generatedPhone);
-        $("[data-test-id=agreement]").click();
-        $x("//span[contains(text(), 'Запланировать')]").click();
-        $("[data-test-id=success-notification]").should(Condition.appear);
-    }
-
-    @Test
-    void validValueTest_ToReplanNotification() {
-        $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id=date] input").setValue(deleteString);
-        $("[data-test-id=date] input").setValue(generatedDate);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
         $("[data-test-id=name] input").setValue("Иван Иванов-Петров");
         $("[data-test-id=phone] input").setValue("+79998887766");
         $("[data-test-id=agreement]").click();
@@ -47,26 +32,58 @@ public class CardPlanningTest {
     }
 
     @Test
-    void validValueTestTo_ReplanlNotificationAppear() {
+    @Order(2)
+    void testTo_ReplanlNotificationAppear() {
         $("[data-test-id=city] input").setValue("Краснодар");
         $("[data-test-id=date] input").setValue(deleteString);
-        $("[data-test-id=date] input").setValue(generatedDate);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(7));
         $("[data-test-id=name] input").setValue("Иван Иванов-Петров");
         $("[data-test-id=phone] input").setValue("+79998887766");
         $("[data-test-id=agreement]").click();
         $x("//span[contains(text(), 'Запланировать')]").click();
         $("[data-test-id=replan-notification]").should(Condition.appear);
         $("[data-test-id=replan-notification] button").click();
-        $("[data-test-id=success-notification]").should(Condition.appear).shouldHave(Condition.text("Встреча успешно запланирована на "));
+        $("[data-test-id=success-notification]").should(Condition.appear).shouldHave(Condition.text("Встреча успешно запланирована на " + DataGenerator.generateDate(7)));
+    }
+
+    @Test
+    @Order(3)
+    void replanInSameWindowTest() {
+        $("[data-test-id=city] input").setValue("Владивосток");
+        $("[data-test-id=date] input").setValue(deleteString);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue("Владимир Васнецов");
+        $("[data-test-id=phone] input").setValue("+79998887799");
+        $("[data-test-id=agreement]").click();
+        $x("//span[contains(text(), 'Запланировать')]").click();
+        $("[data-test-id=success-notification]").should(Condition.appear);
+        $("[data-test-id=date] input").setValue(deleteString);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(7));
+        $x("//span[contains(text(), 'Запланировать')]").click();
+        $("[data-test-id=replan-notification]").should(Condition.appear);
+        $("[data-test-id=replan-notification] button").click();
+        $("[data-test-id=success-notification]").should(Condition.appear).shouldHave(Condition.text("Встреча успешно запланирована на " + DataGenerator.generateDate(7)));
+    }
+
+    @Test
+    void validValueTestWithFaker() {
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").setValue(deleteString);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $x("//span[contains(text(), 'Запланировать')]").click();
+        $("[data-test-id=success-notification]").should(Condition.appear);
     }
 
     @Test
     void invalidCity() {
         $("[data-test-id=city] input").setValue("dfsfd23r23f");
         $("[data-test-id=date] input").setValue(deleteString);
-        $("[data-test-id=date] input").setValue(generatedDate);
-        $("[data-test-id=name] input").setValue(generatedName);
-        $("[data-test-id=phone] input").setValue(generatedPhone);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $x("//span[contains(text(), 'Запланировать')]").click();
         $("[data-test-id=city].input_invalid .input__sub").shouldBe(Condition.exactText("Доставка в выбранный город недоступна"));
@@ -76,9 +93,9 @@ public class CardPlanningTest {
     void emptyCity() {
         $("[data-test-id=city] input").setValue("");
         $("[data-test-id=date] input").setValue(deleteString);
-        $("[data-test-id=date] input").setValue(generatedDate);
-        $("[data-test-id=name] input").setValue(generatedName);
-        $("[data-test-id=phone] input").setValue(generatedPhone);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $x("//span[contains(text(), 'Запланировать')]").click();
         $("[data-test-id=city].input_invalid .input__sub").shouldBe(Condition.exactText("Поле обязательно для заполнения"));
@@ -86,11 +103,11 @@ public class CardPlanningTest {
 
     @Test
     void invalidName() {
-        $("[data-test-id=city] input").setValue("Краснодар");
+        $("[data-test-id=city] input").setValue(validUser.getCity());
         $("[data-test-id=date] input").setValue(deleteString);
-        $("[data-test-id=date] input").setValue(generatedDate);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
         $("[data-test-id=name] input").setValue("Ivan Petrov");
-        $("[data-test-id=phone] input").setValue(generatedPhone);
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $x("//span[contains(text(), 'Запланировать')]").click();
         $("[data-test-id=name].input_invalid .input__sub").shouldBe(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
@@ -98,11 +115,11 @@ public class CardPlanningTest {
 
     @Test
     void emptyName() {
-        $("[data-test-id=city] input").setValue("Краснодар");
+        $("[data-test-id=city] input").setValue(validUser.getCity());
         $("[data-test-id=date] input").setValue(deleteString);
-        $("[data-test-id=date] input").setValue(generatedDate);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
         $("[data-test-id=name] input").setValue("");
-        $("[data-test-id=phone] input").setValue(generatedPhone);
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $x("//span[contains(text(), 'Запланировать')]").click();
         $("[data-test-id=name].input_invalid .input__sub").shouldBe(Condition.exactText("Поле обязательно для заполнения"));
@@ -111,10 +128,10 @@ public class CardPlanningTest {
 
     @Test
     void invalidDate() {
-        $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id=name] input").setValue(generatedName);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=name] input").setValue(validUser.getName());
         $("[data-test-id=date] input").setValue(deleteString);
-        $("[data-test-id=phone] input").setValue(generatedPhone);
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $x("//span[contains(text(), 'Запланировать')]").click();
         $("[data-test-id=date] .input_invalid .input__sub").shouldBe(Condition.exactText("Неверно введена дата"));
@@ -122,11 +139,11 @@ public class CardPlanningTest {
 
     @Test
     void invalidDateSecond() {
-        $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id=name] input").setValue(generatedName);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=name] input").setValue(validUser.getName());
         $("[data-test-id=date] input").setValue(deleteString);
         $("[data-test-id=date] input").setValue("01.01.0001");
-        $("[data-test-id=phone] input").setValue(generatedPhone);
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $x("//span[contains(text(), 'Запланировать')]").click();
         $("[data-test-id=date] .input_invalid .input__sub").shouldBe(Condition.exactText("Заказ на выбранную дату невозможен"));
@@ -134,10 +151,10 @@ public class CardPlanningTest {
 
     @Test
     void emptyPhone() {
-        $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id=name] input").setValue(generatedName);
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=name] input").setValue(validUser.getName());
         $("[data-test-id=date] input").setValue(deleteString);
-        $("[data-test-id=date] input").setValue(date.threeDaysAfter()).pressEscape();
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3)).pressEscape();
         $("[data-test-id=phone] input").setValue("");
         $("[data-test-id=agreement]").click();
         $x("//span[contains(text(), 'Запланировать')]").click();
@@ -146,22 +163,22 @@ public class CardPlanningTest {
 
     @Test
     void checkboxMissed() {
-        $("[data-test-id=city] input").setValue("Краснодар");
+        $("[data-test-id=city] input").setValue(validUser.getCity());
         $("[data-test-id=date] input").setValue(deleteString);
-        $("[data-test-id=date] input").setValue(generatedDate);
-        $("[data-test-id=name] input").setValue(generatedName);
-        $("[data-test-id=phone] input").setValue(generatedPhone);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $x("//span[contains(text(), 'Запланировать')]").click();
         $("[data-test-id=agreement].input_invalid").should(Condition.exist);
     }
 
     @Test
     void notificationXMark() {
-        $("[data-test-id=city] input").setValue("Краснодар");
+        $("[data-test-id=city] input").setValue(validUser.getCity());
         $("[data-test-id=date] input").setValue(deleteString);
-        $("[data-test-id=date] input").setValue(generatedDate);
-        $("[data-test-id=name] input").setValue(generatedName);
-        $("[data-test-id=phone] input").setValue(generatedPhone);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $x("//span[contains(text(), 'Запланировать')]").click();
         $("[data-test-id=success-notification]").should(Condition.appear, Duration.ofMillis(14000));
@@ -170,15 +187,15 @@ public class CardPlanningTest {
 
     @Test
     void notificationValue() {
-        $("[data-test-id=city] input").setValue("Краснодар");
+        $("[data-test-id=city] input").setValue(validUser.getCity());
         $("[data-test-id=date] input").setValue(deleteString);
-        $("[data-test-id=date] input").setValue(date.threeDaysAfter());
-        $("[data-test-id=name] input").setValue(generatedName);
-        $("[data-test-id=phone] input").setValue(generatedPhone);
+        $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $x("//span[contains(text(), 'Запланировать')]").click();
         $("[data-test-id=success-notification]").should(Condition.appear, Duration.ofMillis(14000));
-        $("[data-test-id=success-notification] .notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + date.threeDaysAfter())).shouldBe(Condition.visible);
+        $("[data-test-id=success-notification] .notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(3))).shouldBe(Condition.visible);
     }
 
 
--- src/test/java/DataGenerator.java
+++ src/test/java/DataGenerator.java
@@ -1,39 +1,60 @@
 import com.github.javafaker.Faker;
+import lombok.Value;
 import lombok.experimental.UtilityClass;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
-import java.util.Random;
 
 @UtilityClass
 public class DataGenerator {
 
-    @UtilityClass
-    public static class registration{
 
-        public int random3or7() {
-            Random r = new Random();
-            int low = 3;
-            int high = 7;
-            return r.nextInt(high - low) + low;
+    public String randomName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        return firstName + " " + lastName;
+    }
+
+    public String randomCity() {
+        Faker fake = new Faker();
+        String[] cities = new String[5];
+
+        cities[0] = "Краснодар";
+        cities[1] = "Ростов-на-Дону";
+        cities[2] = "Владивосток";
+        cities[3] = "Москва";
+        cities[4] = "Санкт-Петербург";
+
+        return cities[fake.number().numberBetween(0, 5)];
+    }
+
+    public String randomPhoneNumber(String locale) {
+        Faker fake = new Faker(new Locale(locale));
+        return fake.phoneNumber().phoneNumber();
+    }
+
+    public String generateDate(int days) {
+
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+
+    public static class registration {
+
+        private registration() {
         }
 
-        public String name() {
-            Faker faker = new Faker(new Locale("ru"));
-            String firstName = faker.name().firstName();
-            String lastName = faker.name().lastName();
-            return firstName + lastName;
+        public static UserInfo generateInfo(String locale) {
+            return new UserInfo(randomCity(), randomName(locale), randomPhoneNumber(locale));
         }
+    }
 
-        public static RegistrationByCardInfo generateInfo(String locale) {
-            Faker faker = new Faker(new Locale(locale));
-            return new RegistrationByCardInfo(
-                    faker.name().fullName(),
-                    faker.phoneNumber().phoneNumber(),
-                    LocalDate.now().plusDays(random3or7()).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
-            );
-        }
-
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
     }
 }
